fa49ee488ee87256a6b6cfec14fd1dac82d3c54f
Elin <elin.simonsson@outlook.com>
2022-10-14 12:29:09 +0200

    added MenuActivity

--- app/src/main/java/com/example/group7/MainActivity.kt
+++ app/src/main/java/com/example/group7/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.group7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
-        //Jag testar att skapa en egen branch
 
-        //Aksel
-        //Lägger till en kommentar
-        //Hej jag heter Elin
+
         
 
     }
