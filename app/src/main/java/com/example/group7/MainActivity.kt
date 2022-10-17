6cadac54a252a101b74da573176087d787b611cf
Elin <elin.simonsson@outlook.com>
2022-10-17 14:42:28 +0200

    temporarily added primo button

--- app/src/main/java/com/example/group7/MainActivity.kt
+++ app/src/main/java/com/example/group7/MainActivity.kt
@@ -14,19 +14,23 @@ class MainActivity : AppCompatActivity() {
         val mcdonaldsButton = findViewById<Button>(R.id.mcDonaldsbutton)
         mcdonaldsButton.setOnClickListener {
             val intent = Intent(this, MenuActivity::class.java)
-            restaurant = "McDonalds"
+            restaurant = "Asian Kitchen menu"
             intent.putExtra("restaurant", restaurant)
             startActivity(intent)
         }
-        val rootButton = findViewById<Button>(R.id.rootButton)
-        rootButton.setOnClickListener {
-            restaurant = "rootmenu"
+        val rootsButton = findViewById<Button>(R.id.rootButton)
+        rootsButton.setOnClickListener {
+            restaurant = "Roots & Soil menu"
             val intent = Intent(this, MenuActivity::class.java)
             intent.putExtra("restaurant", restaurant)
             startActivity(intent)
         }
-
-
+        val primoButton = findViewById<Button>(R.id.primoButton).setOnClickListener {
+            restaurant = "Primo Ciao Ciao menu"
+            val intent = Intent(this, MenuActivity::class.java)
+            intent.putExtra("restaurant", restaurant)
+            startActivity(intent)
+        }
 
         
 
