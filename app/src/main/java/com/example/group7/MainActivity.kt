51b94fafc556f15f62931251829c02d2636aadad
Aksel Nielsen <akselnielsen@MacBook-Pro-som-tillhor-Aksel.local>
2022-10-17 12:06:16 +0200

    Hämtar meny till lista

--- app/src/main/java/com/example/group7/MainActivity.kt
+++ app/src/main/java/com/example/group7/MainActivity.kt
@@ -10,12 +10,14 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.getField
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var testButton: Button
 
 
     val db = Firebase.firestore
+    var name = "McDonalds"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,95 +29,44 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        var name = "McDonalds"
+
         //Aksels Branch
 
-        var menu = initializeMenu(name)
 
-
-
-
-
-        for (items in menu){
-            Log.d("!!!","${items.name}")
+    readData() {
+        for(names in it){
+            Log.d("!!!",names.name.toString())
         }
+    }
 
 
 
 
 
-    //  val cities = db.collection("cities")
-
-    //  val data1 = hashMapOf(
-    //      "name" to "San Fransisco",
-    //      "state" to "CA",
-    //      "country" to "USA",
-    //      "capital" to false,
-    //      "population" to 860000,
-    //      "regions" to listOf("west_coast", "norcal")
-    //  )
-    //  cities.document("SF").set(data1)
-
-    //  val data2 = hashMapOf(
-    //      "name" to "Washington D.C",
-    //      "state" to null,
-    //      "country" to "USA",
-    //      "capital" to true,
-    //      "population" to 680000,
-    //      "regions" to listOf("east_coast")
-    //  )
-    //  cities.document("DC").set(data2)
-
-    //   val docRef = db.collection("cities").document("SF")
-    //   docRef.get()
-    //       .addOnSuccessListener { document ->
-    //           if (document != null){
-    //               Log.d("!!!", "DocumentSnapShotData : ${document.data}")
-    //           }
-    //           else{
-    //               Log.d("!!!", "No such document")
-    //           }
-    //       }
-    //       .addOnFailureListener{ exception ->
-    //           Log.d("!!!", "get failed with" , exception)
-
-    //       }
-       // db.collection(restaurantName)
-//
-       //     .get()
-       //     .addOnSuccessListener { documents ->
-       //         for (document in documents) {
-       //             Log.d("!!!", "${document.id} => ${document.data}")
-       //         }
-       //     }
-       //     .addOnFailureListener { exception ->
-       //         Log.w("!!!", "Error getting documents: ", exception)
-       //     }
 
 
 
 
     }
-    fun initializeMenu(name:String) : MutableList<MenuItem>{
-        val menu = mutableListOf<Map>()
+    fun readData(myCallback : (List<MenuItem>) -> Unit){
         db.collection(name)
-
-            .get()
-            .addOnSuccessListener { documentSnapshot ->
-                for (documents in documentSnapshot){
-                    val menuItem = documents.data
-
-                    menu.add(menuItem)
-
+            .get().addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    val list = mutableListOf<MenuItem>()
+                    for (document in task.result){
+                        val name = document.data["name"].toString()
+                        val price = document.data["price"].toString().toInt()
+                        val menuItem = MenuItem(name,price)
+                        list.add(menuItem)
+                    }
+                    myCallback(list)
                 }
 
+
             }
-            .addOnFailureListener{
-                menu.add(MenuItem("This restaurant has no menu",0))
-            }
-        return menu
     }
 
 
-    
+
+
 }
