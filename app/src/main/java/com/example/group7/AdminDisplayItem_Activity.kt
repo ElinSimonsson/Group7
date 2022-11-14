0a3d2b8be87fd20c1afd9625bf2cb2fe16236823
AkselLofstedtNielsen <aksel98@outlook.com>
2022-11-14 11:22:00 +0100

    admin fix

--- app/src/main/java/com/example/group7/AdminDisplayItem_Activity.kt
+++ app/src/main/java/com/example/group7/AdminDisplayItem_Activity.kt
@@ -30,7 +30,6 @@ class AdminDisplayItem_Activity : AppCompatActivity() {
     lateinit var editItemName: EditText
     lateinit var editItemPrice: EditText
     lateinit var editItemImage: ImageView
-    lateinit var selectImageBtn: Button
     lateinit var newImage: String
     lateinit var db: FirebaseFirestore
 
@@ -45,7 +44,6 @@ class AdminDisplayItem_Activity : AppCompatActivity() {
         editItemName = findViewById(R.id.editItemName)
         editItemPrice = findViewById(R.id.editItemPrice)
         editItemImage = findViewById(R.id.editImageView)
-        selectImageBtn = findViewById(R.id.selectImageBtn)
         val saveBtn = findViewById<Button>(R.id.saveBtn)
         val deleteBtn = findViewById<Button>(R.id.deleteBtn)
         val switch = findViewById<Switch>(R.id.switch1)
@@ -90,7 +88,7 @@ class AdminDisplayItem_Activity : AppCompatActivity() {
                                 Toast.makeText(this, "Image Uploaded", Toast.LENGTH_SHORT).show()
                                 Log.d("!!!", "url : $newImage")
                             } else {
-                                //om det inte fungerar ??
+                                Log.d("!!!","Gick inte att ladda upp")
 
                             }
 
@@ -104,15 +102,14 @@ class AdminDisplayItem_Activity : AppCompatActivity() {
 
         //only showing when adding a new item and not displaying it
         switch.isVisible = false
-        selectImageBtn.isVisible = false
         //add drink or food
 
         if (fabNumber == 1) {
             switch.isVisible = true
             var type = "menu"
 
-            selectImageBtn.isVisible = true
-            selectImageBtn.setOnClickListener {
+
+            editItemImage.setOnClickListener {
                 //startar telefonens "image/galleri" och startar en result launcher som inväntar en bild
                 val intent = Intent()
                 intent.type = "image/*"
